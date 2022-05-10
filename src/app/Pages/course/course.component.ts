6c32d772d01f00b6c39fc6ab7e6b9936d6fb0374
Divya <divyashree@amazeinc.in>
2022-05-10 16:06:22 +0530

    dddd

--- src/app/Pages/course/course.component.ts
+++ src/app/Pages/course/course.component.ts
@@ -86,13 +86,13 @@ export class CourseComponent implements OnInit {
     debugger
     if (this.name == undefined || this.description == undefined || this.categoryName == undefined ||
       this.Course_Photo == undefined || this.duration == undefined || this.entryCriteria ==
-      undefined || this.exitCriteria == undefined || this.categoryid == undefined || this.trainingType
+      undefined || this.exitCriteria == undefined  || this.trainingType
       == undefined) {
       Swal.fire("Please fill all the fields");
     }
     else {
       var json = {
-        "categoryName": this.categoryName,
+        // "categoryName": this.categoryName,
         "name": this.name,
         "description": this.description,
         "photo": this.Course_Photo,
@@ -100,7 +100,7 @@ export class CourseComponent implements OnInit {
         "fee":this.fee,
         "entryCriteria": this.entryCriteria,
         "exitCriteria": this.exitCriteria,
-        // "CategoryID": this.categoryid,
+         "CategoryID": this.categoryid,
         "trainingLink": this.trainingLink,
         "userName": this.userName,
         "password": this.password,
@@ -121,7 +121,7 @@ export class CourseComponent implements OnInit {
     debugger
     var json = {
       'ID': this.id,
-      "categoryName": this.categoryName,
+      // "categoryName": this.categoryName,
       "name": this.name,
       "description": this.description,
       "photo": this.Course_Photo,
