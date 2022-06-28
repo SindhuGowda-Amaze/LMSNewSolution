7aceca91cbb0697c7c0bed39e31a16cf8110802a
SindhuGowda-Amaze <you@example.com>
2022-06-28 13:00:02 +0530

    dd

--- src/app/learning.service.ts
+++ src/app/learning.service.ts
@@ -7,7 +7,7 @@ import { AnyCatcher } from 'rxjs/internal/AnyCatcher';
 })
 export class LearningService {
   // public baseURL = "http://localhost:4199/";
-  public baseURL = "http://103.133.214.197/LearningandDevelopment/";
+  public baseURL = "http://103.133.214.197/LearningandDevelopmentdemo/";
   // public host1 = "https://digioffice.amazeone.co/digiofficeapi";
   public host = "https://digioffice.amazeone.co/DigiOfficeAPI"
   
